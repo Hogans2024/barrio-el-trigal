b890364afb5e538cfeec3435d86df63ffda8fcce
Hogans2024 <Hogans2024@users.noreply.github.com>
2026-07-22 17:39:37 -0700

    v0.1.81 al presionar boton gris, texto superior se vuelve blanco sin efecto

--- src/components/ActiveAlarmModal.tsx
+++ src/components/ActiveAlarmModal.tsx
@@ -367,7 +367,7 @@ export default function ActiveAlarmModal({ isOpen, onClose, type }: ActiveAlarmM
                     <span className="text-[11px] text-[#FFD700] font-mono font-bold">Vecino Autorizado: 12345678</span>
                   </div>
                   <div className="w-full flex items-center justify-center text-gray-400 text-[11px] sm:text-xs leading-normal px-0">
-                    <span className={`whitespace-nowrap uppercase ${enteredPin.length < 1 ? 'text-[#FFD700] animate-pulse' : 'text-white'}`}>⚠️ PRIMERO DIGITE SU NUMERO DE CELULAR</span>
+                    <span className={`whitespace-nowrap uppercase ${enteredPin.length < 1 && !showMissingPinAlert ? 'text-[#FFD700] animate-pulse' : 'text-white'}`}>⚠️ PRIMERO DIGITE SU NUMERO DE CELULAR</span>
                   </div>
                 </div>
               ) : (
